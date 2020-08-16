4eebd0b0dfe472363af87d75b3227a72d4926e89
GGLozanov <glozanov15@gmail.com>
2020-08-16 19:56:46 +0300

    [Update] Added JJWT dependency, envvar config, FragmentPagerAdapter, & UserListViewAdapter

--- app/src/main/java/com/example/authio/adapters/FragmentPagerAdapter.java
+++ app/src/main/java/com/example/authio/adapters/FragmentPagerAdapter.java
@@ -1,4 +1,36 @@
 package com.example.authio.adapters;
 
-public class FragmentPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentPagerAdapter extends FragmentStatePagerAdapter {
+    private List<Fragment> fragments = new ArrayList<>();
+
+    public FragmentPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    public FragmentPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    public void addFragment(Fragment fragment) {
+        fragments.add(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
 }
--- app/src/main/java/com/example/authio/adapters/UserListViewAdapter.java
+++ app/src/main/java/com/example/authio/adapters/UserListViewAdapter.java
@@ -1,4 +1,77 @@
 package com.example.authio.adapters;
 
-public class UserListViewAdapter {
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.authio.R;
+import com.example.authio.databinding.SingleUserBinding;
+import com.example.authio.models.User;
+import com.example.authio.viewmodels.ProfileFragmentViewModel;
+
+import java.util.List;
+
+public class UserListViewAdapter extends ArrayAdapter<User> {
+    private List<User> users;
+
+    public UserListViewAdapter(Context context, int resource, List<User> users) {
+        super(context, resource, users);
+        this.users = users;
+    }
+
+    @Override
+    public int getCount() {
+        return users.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return users.get(position).hashCode();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        /**
+         * Credit to @sergi from https://stackoverflow.com/questions/33943717/android-data-binding-with-custom-adapter
+         * for the implementation of data-binding within this method using convertView's tag to assign data binding instance
+         */
+        SingleUserBinding singleUserBinding;
+
+        if(convertView == null)  {
+            singleUserBinding = DataBindingUtil.inflate(
+                    LayoutInflater.from(getContext()), // has access to context. . . somehow?
+                    R.layout.single_user,
+                    parent,
+                    false
+            );
+
+            /* SingleUserBinding.inflate(
+                    layoutInflater,
+                    parent,
+                    false); // other optional inflation */
+
+            convertView = singleUserBinding.getRoot();
+        } else {
+            singleUserBinding = (SingleUserBinding) convertView.getTag();
+        }
+
+        User user = getItem(position);
+
+        singleUserBinding.setImmutableUser(user);
+
+        convertView.setTag(singleUserBinding);
+
+        return convertView;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+        notifyDataSetChanged();
+    }
 }
